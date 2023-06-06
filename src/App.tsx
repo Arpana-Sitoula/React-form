2d13d61e5af64087b430fda9f8830902cd21f4a9
Arpana <arp2ctaula@gmail.com>
2023-06-06 11:47:33 +0545

    regex implemnted

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,14 @@
 import './App.css'
-import { useQuery, useMutation } from 'react-query'
 import ChakraForm from './ui/atoms/ChakraForm'
-import Form from './ui/atoms/Form'
 
-const POST = [
-  {id: 1, title:"post 1"},
-  {id: 2, title:"post 2"},
-]
+
 function App() {
-const postQuery = useQuery({
-  queryKey: ["posts"],
-  queryFn:() => wait(1000).then(()=>[...POST]),
-})
-
-if(postQuery.isLoading){
-  return <h1>Loading...</h1>
-}
   return (
     <>
-      {postQuery.data?.map(post=>{
-       return <div key={post.id}>{post.title}</div>
-      })}
-     <Form/>
      <ChakraForm/>
     </>
   )
 }
 
-function wait(duration: number){
-  return new Promise(resolve => setTimeout(resolve, duration))
-}
 
 export default App
--- src/ui/atoms/ChakraForm.tsx
+++ src/ui/atoms/ChakraForm.tsx
@@ -1,30 +1,79 @@
-import { Button, FormControl, FormErrorMessage, FormLabel, Input } from '@chakra-ui/react'
-import {useForm, SubmitHandler} from 'react-hook-form'
+import {
+  Button,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input,
+  Select,
+} from "@chakra-ui/react";
+import { useForm, SubmitHandler } from "react-hook-form";
 
 type Inputs = {
-    name: string,
-    email: string,
-    phone: number,
-    gender: string,
+  name: string;
+  email: string;
+  phone: number;
+  gender: string;
 };
 
 const ChakraForm = () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    console.log(data);
+    reset();
+  };
 
-const {register, handleSubmit, reset, formState:{errors}}  = useForm<Inputs>() ;
-const onSubmit: SubmitHandler<Inputs> = data => 
-{console.log(data);
-reset()};
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <FormControl isRequired isInvalid={errors?.name ? true : false}>
+        <FormLabel>Name</FormLabel>
+        <Input
+          type="text"
+          {...register("name", { required: true, maxLength: 20, minLength: 5 })}
+        />
+        {errors.name && <FormErrorMessage>{errors.name.type}</FormErrorMessage>}
+      </FormControl>
 
-    return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <FormControl isRequired isInvalid={errors?.name ? true:false}>
-                <FormLabel>Name</FormLabel>
-                <Input type='text' {...register('name', {required: true, maxLength: 20,minLength:5})}/>
-                {errors?.name && <FormErrorMessage>{errors.name.type}</FormErrorMessage>}
-            </FormControl>
-            <Button type='submit'>Submit</Button>
-        </form>
-    )
-}
+      <FormControl isRequired isInvalid={errors?.email ? true : false}>
+        <FormLabel>Email</FormLabel>
+        <Input
+          type="email"
+          {...register("email", {
+            required: true,
+            pattern: new RegExp(
+              /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$/
+            ),
+          })}
+        />
+        {errors.email?.type === "required" && (
+          <FormErrorMessage>Email is required</FormErrorMessage>
+        )}
+        {errors.email?.type === "pattern" && (
+          <FormErrorMessage>Invalid email address</FormErrorMessage>
+        )}
+      </FormControl>
 
-export default ChakraForm
+      <FormControl isInvalid={errors?.phone ? true : false}>
+        <FormLabel>Phone number</FormLabel>
+        <Input
+          {...register("phone", {
+            pattern: new RegExp(
+              /^\+?[0-9]{1,3}[-. (]?\d{1,3}[-. )]?[-. ]?\d{1,4}[-. ]?\d{1,4}$/
+            ),
+          })}
+        />
+        {errors.phone?.type === "pattern" && (
+          <FormErrorMessage>Invalid phone number</FormErrorMessage>
+        )}
+      </FormControl>
+
+      <Button type="submit">Submit</Button>
+    </form>
+  );
+};
+
+export default ChakraForm;
